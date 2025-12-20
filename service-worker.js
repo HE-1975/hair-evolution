2674de22f8d32428b01172ea88997d542c187423
HE-1975 <2200054@fortec.edu.br>
2025-12-20 12:33:41 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,28 @@
-self.addEventListener("install", e => {
-    e.waitUntil(
-        caches.open("hair-cache").then(cache =>
-            cache.addAll([
-                "index.html",
-                "adm.html",
-                "agenda.html",
-                "relatorio.html",
-                "style.css",
-                "script.js"
-            ])
-        )
-    );
+const CACHE_NAME = "hair-evolution-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./adm.html",
+  "./agenda.html",
+  "./relatorio.html",
+  "./style.css",
+  "./script.js",
+  "./adm.js",
+  "./agenda.js",
+  "./supabase.js",
+  "./logo.png",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", e => {
-    e.respondWith(
-        caches.match(e.request).then(resp => resp || fetch(e.request))
-    );
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
 });
-
