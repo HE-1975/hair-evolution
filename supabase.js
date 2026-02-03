11168de9330c299c502266c5df7cbb3258f4b898
HE-1975 <2200054@fortec.edu.br>
2026-02-03 09:20:31 -0300

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://fbhcmomiwezntpwmckgw.supabase.co",
-  "sb_publishable_kJNOi5iHNDuyireXGr6nnw_LgPo3BFC"
-);
